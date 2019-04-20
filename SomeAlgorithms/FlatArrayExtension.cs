5fe5de66b0f377eb97f2c508f51073c7c86510c3
unknown <juampavilla@gmail.com>
2019-04-20 02:09:52 -0300

    initial commit

---
+++ SomeAlgorithms/FlatArrayExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SomeAlgorithms
+{
+    /// <summary>
+    /// extension for array that allows flatten an array
+    /// </summary>
+    public static class FlatArrayExtensionRecursive 
+    {
+        private static T[] Concat<T>(this T[] a, T[] b)
+        {
+            T[] result = new T[a.Length + b.Length];
+            Array.Copy(a, result, a.Length);
+            Array.Copy(b, 0, result, a.Length, b.Length);
+            return result;
+        }
+
+        /// <summary>
+        /// recursive method that ask about if the next element is a value or array
+        /// object param allow this overhead
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        static private object[] GetFlatArrayRecursive(object array, int i)
+        {
+            if (((object[])array).Length == i)
+            {
+                return new object[] { };
+            }
+            else
+            {
+                Type array_i_type = (((object[])array)[i]).GetType();
+                if (array_i_type == typeof(object[]))
+                {
+                    return GetFlatArrayRecursive((((object[])array)[i]), 0).
+                            Concat(GetFlatArrayRecursive(array, i + 1)).
+                                                     ToArray();
+
+                }
+                else
+                {
+                    return (new[] { (((object[])array)[i]) }).
+                        Concat(GetFlatArrayRecursive(array, i + 1)).
+                        ToArray();
+                }
+             }
+        }
+
+        static public object[] GetFlatArray(this object array)
+        {
+            return GetFlatArrayRecursive(array, 0);
+        }      
+    }
+}
---
+++ SomeAlgorithms/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SomeAlgorithms
+{
+    class Program
+    {
+        static public void WriteArray(object[] array, string name)
+        {
+
+            Console.Write(name + " array: [ ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i != 0) { Console.Write(","); }
+                Console.Write(" " + array[i].ToString() + " ");
+            }
+            Console.Write(" ]");
+        }
+
+
+        static void Main(string[] args)
+        {
+            /*Write some code, that will flatten an array
+             * of arbitrarily nested arrays of integers into 
+             * a flat array of integers.
+             * e.g. [[1,2,[3]],4] -> [1,2,3,4]. *
+            */
+
+            Console.WriteLine("Input 1: [[1,2,[3]],4]");         
+            object[] array1 = new object[] { new object[] { 1, 2, new object[] { 3 } }, new object[] { 4 } };
+            var flatArray1 = array1.GetFlatArray();
+            WriteArray(flatArray1, "Flat 1");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Input 2: [[1,2,[3]],4, [hi,bye,[5]] ]");
+            object[] array2 = new object[] { new object[] { 1, 2, new object[] { 3 } }, new object[] { 4 },
+                              new object[] {"hi","bye",new object[] { 5 } } };
+            var flatArray2 = array2.GetFlatArray();
+            WriteArray(flatArray2, "Flat 2");
+
+
+            Console.ReadLine();
+        }
+    }
+}
